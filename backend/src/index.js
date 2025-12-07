0b8811a592f15cfa5c5dc2cc9acc79d4c023ac12
Srishti Srivastava <srishtisri224@gmail.com>
2025-12-07 17:23:21 +0530

    add mongodb conenction

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,38 @@
-import express from 'express';
-const app = express()
-let port = 4000;
+// require('dotenv').config({path: './env'});
+import dotenv from 'dotenv';
+dotenv.config({path:'./env'});
 
-app.get("/", (req,res)=>{
-    res.send("Hello World!");
-})
 
-app.listen(port,()=>{
-    console.log(`Server is running at ${port}`);
-})
+//SECOND APPROACH
+
+import connectDB from './db/check.js';
+connectDB();
+
+
+
+//FIRST APPROACH
+
+// import mongoose from 'mongoose';
+// import { DB_NAME } from './constants';
+// import express from 'express';
+// const app = express()
+// //we are making an IIFE over here
+// ; (async function connectDB(){
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.log("Error: ",error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//     console.log(`Server is running at ${process.env.PORT}`);
+// })
+//     }
+//     catch(error){
+//         console.log("Error: ",error);
+//         throw error;
+        
+//     }
+// })();
+
